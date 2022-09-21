f9f8d2905e4440093535a7cff4b94ff2ad6a235c
sumitchauhanJan <sumit.chauhan@deliveryhero.com>
2022-09-21 17:24:59 +0200

    Created view for theatre booking

---
+++ src/main/kotlin/com/virtualprogrammers/theater/control/MainController.kt
@@ -0,0 +1,23 @@
+package com.virtualprogrammers.theater.control
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
+
+@Controller
+class MainController{
+
+    @RequestMapping("")
+    fun homePage():ModelAndView=
+         ModelAndView("seatBooking","bean",checkAvailabilityBackingBean())
+
+}
+
+class checkAvailabilityBackingBean(){
+    val seatNums=1..36
+    val seatRows='A'..'O'
+    var selectedSeatNum:Int=1
+    var selectedSeatRow:Char='A'
+    var result:String=" "
+
+}
